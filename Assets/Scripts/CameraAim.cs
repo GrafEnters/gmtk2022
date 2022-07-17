dacf6ebb32c5f40e6cba56f88382508f56d14d1f
georgy.polishchuk <georgy.polishchuk@kosmosteam.com>
2022-07-17 15:52:56 +0300

    Clipping tryFix

--- Assets/Scripts/CameraAim.cs
+++ Assets/Scripts/CameraAim.cs
@@ -6,32 +6,105 @@ using UnityEngine;
 public class CameraAim : MonoBehaviour {
     public float POVNormal = 40, POVAIM = 20;
     public float lerpSpeed = 0.3f;
-    
-    [Header("Other")]   
+
+    public float minDistance = 0.3f;
+    public float minRotation = 3f;
+
+    public float PresitionMultiplierInAim = 3;
+    public float SensivityLowerInAim = 0.3f;
+    public float ClippingFixTries;
+
+    public bool isLockAim;
+    public LayerMask clipMask;
+
+    [Header("Other")]
     public Transform NormalMode;
+
     public Transform AimMode;
     private Transform cameraTransform;
     private Camera camera;
     private float targetPov;
-   
- 
+
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+
+    public MouseLook verticalCameraRotation, horizontalCameraRotation;
+
+    private bool isAim;
+    private Vector3 clipOffset;
+
     private void Awake() {
         cameraTransform = Camera.main.transform;
         camera = Camera.main;
     }
-    
-    void Update()
-    {
-        if (Input.GetMouseButton(1)) {
-            targetPov = POVAIM;
-            cameraTransform.SetParent(AimMode);
+
+    private void Update() {
+        if (isLockAim) {
+            if (Input.GetMouseButtonDown(1)) {
+                isAim = !isAim;
+            }
         } else {
-            targetPov = POVNormal;
-            cameraTransform.SetParent(NormalMode);
+            isAim = Input.GetMouseButton(1);
         }
 
-        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, targetPov,lerpSpeed);
-        cameraTransform.localRotation = Quaternion.Lerp( cameraTransform.localRotation, Quaternion.identity, lerpSpeed);
-        cameraTransform.localPosition = Vector3.Lerp( cameraTransform.localPosition, Vector3.zero, lerpSpeed);
+        
     }
+
+    void FixedUpdate() {
+        Follow();
+    }
+
+    private void Follow() {
+        if (isAim) {
+            targetPov = POVAIM;
+            targetPosition = AimMode.position;
+            targetRotation = AimMode.rotation;
+            verticalCameraRotation.SensivityMultiplier = SensivityLowerInAim;
+            horizontalCameraRotation.SensivityMultiplier = SensivityLowerInAim;
+        } else {
+            targetPov = POVNormal;
+            targetPosition = NormalMode.position;
+            targetRotation = NormalMode.rotation;
+            verticalCameraRotation.SensivityMultiplier = 1;
+            horizontalCameraRotation.SensivityMultiplier = 1;
+        
+        }
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, targetPov, lerpSpeed);
+
+        TryFixClip();
+        
+        if (minDistance / (isAim ? PresitionMultiplierInAim : 1) <
+            Vector3.Distance(cameraTransform.position, targetPosition))
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetPosition + clipOffset, lerpSpeed * (isAim? 2: 1));
+        if (minRotation / (isAim ? PresitionMultiplierInAim : 1) <
+            Quaternion.Angle(cameraTransform.rotation, targetRotation))
+            cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, targetRotation, lerpSpeed * (isAim? 2 : 1));
+    }
+
+    private void TryFixClip() {
+        bool isClip = Physics.OverlapSphere(targetPosition, 0.2f,clipMask).Length > 0;
+        if(!isClip) {
+            clipOffset = Vector3.zero;
+            return;
+        }
+        
+        
+        for (int i = 0; i < ClippingFixTries; i++) {
+            Vector3 dir = PlayerController.BodyPosition - cameraTransform.position;
+            targetPosition += dir * 1 / ClippingFixTries;
+            isClip = Physics.OverlapSphere(targetPosition, 0.2f,clipMask).Length > 0;
+            if (!isClip)
+               return;
+        }
+       /*clipOffset = Vector3.zero;
+        for (int i = 0; i < ClippingFixTries; i++) {
+            Vector3 dir = PlayerController.BodyPosition - cameraTransform.position;
+            clipOffset += dir * -1;
+            isClip = Physics.OverlapSphere(targetPosition + clipOffset, 0.1f).Length > 0;
+            if (!isClip)
+                return;
+        }*/
+    }
+    
+    
 }
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -3,21 +3,23 @@ using System.Collections;
 
 /// MouseLook rotates the transform based on the mouse delta.
 /// Minimum and Maximum values can be used to constrain the possible rotation
-
 /// To make an FPS style character:
 /// - Create a capsule.
 /// - Add the MouseLook script to the capsule.
 ///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
 /// - Add FPSInputController script to the capsule
 ///   -> A CharacterMotor and a CharacterController component will be automatically added.
-
 /// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
 /// - Add a MouseLook script to the camera.
 ///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public class MouseLook : MonoBehaviour {
+    public enum RotationAxes {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
 
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
     public float sensitivityX = 15F;
     public float sensitivityY = 15F;
@@ -30,41 +32,35 @@ public class MouseLook : MonoBehaviour {
 
     float rotationY = 0F;
 
-  
-   // public Transform
-    void Update ()
-    {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+    [HideInInspector]
+    public float SensivityMultiplier = 1;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+    // public Transform
+    void FixedUpdate() {
+        if (axes == RotationAxes.MouseXAndY) {
+            float rotationX = transform.localEulerAngles.y +
+                              Input.GetAxis("Mouse X") * sensitivityX * SensivityMultiplier;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY * SensivityMultiplier;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
             transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
+        } else if (axes == RotationAxes.MouseX) {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+        } else {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY * SensivityMultiplier;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
         }
-
-      
     }
 
-    void Start ()
-    {
-    //if(!networkView.isMine)
+    void Start() {
+        //if(!networkView.isMine)
         //enabled = false;
 
         // Make the rigid body not change rotation
         //if (rigidbody)
-            //rigidbody.freezeRotation = true;
+        //rigidbody.freezeRotation = true;
     }
 }
